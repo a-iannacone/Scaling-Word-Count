a5f268f18dd6a8c4b38621a4f66bf1a237f87f0a
Allen Iannacone <allen.iannacone@gmail.com>
2022-02-06 21:08:47 -0500

    update with docker files

---
+++ deploy/TF/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/allen/Downloads/count-app-340600-db2c46878f92.json")
+
+  project = "count-app-340600"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
---
+++ deploy/TF/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "count-app-340600"
+region     = "us-central1"
+credentials = file("C:/Users/allen/Downloads/count-app-340600-db2c46878f92.json")
